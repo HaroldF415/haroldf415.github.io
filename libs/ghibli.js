92ccbdce76a0e8f474e7406b7d88c99050c1a2b9
Harold Fachin <harold.f415@gmail.com>
2020-05-12 08:01:35 -0400

    blog entry May 12th, 2020

--- libs/ghibli.js
+++ libs/ghibli.js
@@ -1,3 +1,14 @@
 /* This script is meant to connect with Studio Ghibli's API and request information about it's films and present it to the user. */
 
-/* STEP 1 - Retrieving */
+// STEP 1 - Retrieving the data with an HttpREQUEST
+// Create a REQUEST variable and assign a new XMLHttpRequest object to it.
+// XMLHttpRequest - object that can be used to REQUEST data from a WEB SERVER.
+
+var request = new XMLHttpRequest()
+
+// Open a new connection, using the GET request on the URL endpoint
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+// The onLoad event will be called when an XMLHttpRequest transaction has been completed successfully
+
